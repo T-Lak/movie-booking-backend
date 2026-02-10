a8f761cb91ae00ffc5ab7ee85e54482ecb9a2159
thelon <jps_90@gmx.de>
2026-02-10 23:22:20 +0100

    added newly created services and controllers to the module.

--- src/shows/shows.module.ts
+++ src/shows/shows.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { ShowsClientService } from './services/shows.client.service';
-import { ShowsClientController } from './controllers/shows.client.controller';
 import { Show } from './entities/show.entity';
+import { ShowsClientController } from './controllers/shows.client.controller';
+import { ShowsAdminController } from './controllers/shows.admin.controller';
+import { ShowsClientService } from './services/shows.client.service';
+import { ShowsAdminService } from './services/shows.admin.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Show])],
-  controllers: [ShowsClientController],
-  providers: [ShowsClientService],
+  controllers: [
+    ShowsClientController,
+    ShowsAdminController,
+  ],
+  providers: [
+    ShowsClientService,
+    ShowsAdminService,
+  ],
 })
 export class ShowsModule {}
