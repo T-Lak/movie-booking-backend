8d8f594e5995eb70b418e50ea4a26d380ea8b73b
thelon <jps_90@gmx.de>
2026-02-13 19:34:42 +0100

    fix bug consider reservations for clean up service

--- src/modules/bookings/services/bookings.cleanup.service.ts
+++ src/modules/bookings/services/bookings.cleanup.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { LessThan, Repository } from 'typeorm';
+import { In, LessThan, Repository } from 'typeorm';
 import { Cron } from '@nestjs/schedule';
 
 import { BookingStatus } from '../enums/bookings-status.enum';
 import { Booking } from '../entity/bookings.entity';
+import { Reservation } from '../entity/reservation.entity';
 
 @Injectable()
 export class BookingsCleanupService {
   constructor(
     @InjectRepository(Booking)
     private readonly bookingRepo: Repository<Booking>,
+    @InjectRepository(Reservation)
+    private readonly reservationRepo: Repository<Reservation>,
   ) {}
 
   @Cron('*/1 * * * *')
@@ -22,13 +25,18 @@ export class BookingsCleanupService {
         status: BookingStatus.PENDING,
         expiresAt: LessThan(now),
       },
+      select: ['id'],
     });
 
     if (expiredBookings.length === 0) return;
 
-    for (const booking of expiredBookings) {
-      booking.status = BookingStatus.CANCELLED;
-    }
+    const ids: number[] = expiredBookings.map(b => b.id);
+
+    await this.bookingRepo.update(ids, { status: BookingStatus.EXPIRED });
+
+    await this.reservationRepo.delete({
+      booking: In(ids)
+    });
 
     await this.bookingRepo.save(expiredBookings);
   }
